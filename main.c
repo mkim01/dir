d2609c8fd0cb9e3e0b3f6e22ca64721b9710e7c4
Minuk Kim <mkim01@stuy.edu>
2019-11-19 01:12:36 -0500

    stat and print the dir

--- main.c
+++ main.c
@@ -9,9 +9,10 @@
 #include <sys/types.h>
 
 int main(int argc, char *argv[]) {
-    DIR *dir;
+  //scan the comman arguments;
+    DIR *dirs;
     if (argc > 1) {
-      dir = opendir(argv[1]);
+      dirs = opendir(argv[1]);
         if (errno == -1) {
           printf("error: %s", strerror(errno));
             return 0;
@@ -25,6 +26,48 @@ int main(int argc, char *argv[]) {
         printf("error: %s", strerror(errno));
         return 0;
     }
+    struct dirent* dir = readdir(dirs);
+
+    //statistics
+    printf("statistics for directory\n");
+    struct stat n;
+    int size;
+    while (dir) {
+        int error = stat(dir->d_name, &n);
+        if (error == -1) {
+            printf("error: %s", strerror(errno));
+            return 0;
+        }
+        else {
+            size += n.st_size;
+        }
+        dir = readdir(dirs);
+    }
+    printf("\ntotal Directory Size: %d Bytes\n", size);
+
+    rewinddir(dirs);
+    dir = readdir(dirs);
+
+    printf("\nDirectories:\n");
+
+    while (dir) {
+        if(dir->d_type == DT_DIR) {
+            printf("\t%s\n", dir->d_name);
+        }
+        dir = readdir(dirs);
+    }
+    rewinddir(dirs);
+    dir = readdir(dirs);
+
+    printf("\nRegular Files:\n");
+
+    while (dir) {
+        if(dir->d_type != DT_DIR) {
+            printf("\t%s\n", dir->d_name);
+        }
+        dir = readdir(dirs);
+    }
+    closedir(dirs);
 
     return 0;
 }
